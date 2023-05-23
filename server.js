a89f1d3d2e5467a4578a53a1c255945b578f86b7
Pdut3 <115908348+Pdut3@users.noreply.github.com>
2023-05-22 20:24:33 -0600

    changes all around

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const path = require("path");
-const apiRoutes = require("./apiRoutes");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require("./routes/apiRoutes");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static("./public"));
-
-// Use the apiRoutes for all routes starting with '/api'
 app.use("/api", apiRoutes);
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => console.log("Server running on PORT", PORT));
+
